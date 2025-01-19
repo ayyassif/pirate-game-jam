06763c94c04674f8189d0505b0b7c55608b98277
ayyassif <ayyassif@student.1337.ma>
2025-01-19 20:08:42 +0100

    fixing funky movement

--- Scripts/States/Idle.gd
+++ Scripts/States/Idle.gd
@@ -6,7 +6,10 @@ func Enter():
 	
 func Update(_delta: float):
 	if Input.get_vector("MoveDown","MoveLeft","MoveRight","MoveUp"):
+		print("hh");
 		Transitioned.emit(self, "Walking")
+	player.velocity.x = 0.0
+	player.velocity.z = 0.0
 
 func Exit():
 	pass
--- Scripts/States/Walking.gd
+++ Scripts/States/Walking.gd
@@ -19,18 +19,18 @@ func Update(_delta: float):
 func _physics_Update(delta: float):
 	player.velocity = Vector3.ZERO
 	
-	var raw_input := Input.get_vector("MoveLeft", "MoveRight", "MoveUp", "MoveDown")
+	var raw_input: Vector2
+	raw_input = Vector2(Input.get_axis("MoveLeft", "MoveRight"), Input.get_axis("MoveUp", "MoveDown"))
+	print("-------------------------")
+	print(raw_input)
 	var direction := (player.transform.basis * Vector3(raw_input.x, 0, raw_input.y)).normalized()
-	
-	player.velocity = player.velocity.move_toward(direction * speed, acceleration)
-	
-	#
-	#if direction:
-		#player.velocity.x = direction.x * speed
-		#player.velocity.z = direction.z * speed
-	#else:
-		#player.velocity.x = move_toward(player.velocity.x, 0, speed)
-		#player.velocity.z = move_toward(player.velocity.z, 0, speed)
+	print(direction)
+	print("-------------------------")
+	if direction:
+		player.velocity.x = direction.x * speed;
+		player.velocity.z = direction.z * speed;
+	#player.velocity = player.velocity.move_toward(direction * speed, acceleration)
+
 	player.move_and_slide()
 	
 	pass
